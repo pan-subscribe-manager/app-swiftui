f5a8db6038d9c69b6be0d0f1343d129fdffc159f
pan93412 <pan93412@gmail.com>
2024-01-10 12:08:17 +0800

    Initial Commit

---
+++ FinanceControl/FinanceControlApp.swift
@@ -0,0 +1,32 @@
+//
+//  FinanceControlApp.swift
+//  FinanceControl
+//
+//  Created by Yi-Jyun Pan on 2024/1/10.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FinanceControlApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ FinanceControl/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  FinanceControl
+//
+//  Created by Yi-Jyun Pan on 2024/1/10.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
