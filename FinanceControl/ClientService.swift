4e2e7e38508f2f6db2ec9ad935667cfb654cb528
pan93412 <pan93412@gmail.com>
2024-01-11 00:54:44 +0800

    Rethrows & Pass details

--- FinanceControl/ClientService.swift
+++ FinanceControl/ClientService.swift
@@ -24,15 +24,15 @@ class ClientService: ObservableObject {
 	
 	/// run is the wrapper of Client, which accepts a closure that calls the Client methods,
 	/// and *logout* for Unauthorized errors.
-	func run<T>(_ op: (Client) async throws -> T) async throws -> T {
+	func run<T>(_ op: (Client) async throws -> T) async rethrows -> T {
 		do {
 			return try await op(client)
-		} catch ClientError.unauthorized {
+		} catch ClientError.unauthorized(let details) {
 			DispatchQueue.main.async { [unowned self] in
 				self.isLoggedIn = false
 			}
 			client.logout()
-			throw ClientError.unauthorized
+			throw ClientError.unauthorized(details: details)
 		}
 	}
 }
